7fd5b84de8020814a5301cffd04abad4d44f515a
Jonas R. Denardi <jonas_denardi@hotmail.com>
2020-01-17 00:35:55 -0300

    Continuação

--- OmniStack_10/src/index.js
+++ OmniStack_10/src/index.js
@@ -1,19 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require("./routes");
 
 const app = express();
-
-mongoose.connect("mongodb+srv://jonas:147258@cluster0-9q0pm.mongodb.net/week10?retryWrites=true&w=majority", 
-{useNewUrlParser: true, useUnifiedTopology: true});
-
-app.use(express.json());
-
-// Tipos de parâmetros do express:
-
- // Query Params: na URL ?. request.query (Filtros, Ordenação, Paginação, ...)
- // Route Params: divisao por /. request.params (Identificar um recurso na alteração ou remoção)
- // Body: Dados para a criação ou alteração de registro
-
 // MongoDB (Não-Relacional)
+mongoose.connect("mongodb+srv://jonas:147258@cluster0-9q0pm.mongodb.net/week10?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.set('useCreateIndex', true);
+app.use(express.json());
+app.use(routes);
 
-app.listen(3333);
+app.listen(3333); 
+
--- OmniStack_10/src/routes.js
+++ OmniStack_10/src/routes.js
@@ -1,10 +1,19 @@
 const {Router} = require('express');
 
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
+
 const routes = Router();
 
-routes.post('/users', (request,response) => {
-    console.log(request.body);
-    return response.json({message: "Hello OmniStack"}); 
-});
+routes.get('/devs', DevController.index);
+routes.post('/devs', DevController.store);
+
+routes.get('/search', SearchController.index)
 
 module.exports = routes;
+
+// Tipos de parâmetros do express:
+
+ // Query Params: na URL ?. request.query (Filtros, Ordenação, Paginação, ...)
+ // Route Params: divisao por /. request.params (Identificar um recurso na alteração ou remoção)
+ // Body: Dados para a criação ou alteração de registro
